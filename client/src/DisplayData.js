ae53a7152c5fae4cbd21d05ccd489a53e28c17dc
Emely Pleitez <epleitez@belaytech.com>
2025-06-19 15:10:56 -0400

    add button for creating a new user and functionality

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useQuery, useLazyQuery, gql } from "@apollo/client";
+import { useQuery, useLazyQuery, gql, useMutation } from "@apollo/client";
 
 const QUERY_ALL_USERS = gql`
   query GetAllUsers {
@@ -32,9 +32,25 @@ const GET_MOVIE_BY_NAME = gql`
   }
 `;
 
+const CREATE_USER_MUTATION = gql`
+  mutation CreateUser($input: CreateUserInput!) {
+    createUser(input: $input) {
+      id
+      name
+      username
+      age
+    }
+  }
+`;
+
 function DisplayData() {
   //state for fetching data button
   const [movieSearched, setMovieSearched] = useState("");
+  //state for creating a user
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
+  const [nationality, setNationality] = useState("");
 
   // Fetching all users and movies using Apollo Client's useQuery hook
   const { data, loading, error } = useQuery(QUERY_ALL_USERS);
@@ -42,17 +58,53 @@ function DisplayData() {
   const [fetchMovie, { data: movieSearchedData, error: movieError }] =
     useLazyQuery(GET_MOVIE_BY_NAME);
 
+  const [createUser] = useMutation(CREATE_USER_MUTATION);
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
   if (movieError) return <p>Error fetching movie: {movieError.message}</p>;
 
   return (
     <div>
+      <div>
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => setName(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Age..."
+          onChange={(event) => setAge(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Nationality..."
+          onChange={(event) => setNationality(event.target.value.toUpperCase())}
+        />
+        <button
+          onClick={() => {
+            createUser({
+              variables: {
+                input: { name, username, age: Number(age), nationality },
+              },
+            });
+          }}
+        >
+          Create User
+        </button>
+      </div>
+      <h2>Users: </h2>
       {data &&
         data.users &&
         data.users.map((user) => (
           <div key={user.id}>
-            <h1>Name: {user.name}</h1>
+            <h3>{user.name}</h3>
             <p>Age: {user.age}</p>
             <p>Username: {user.username}</p>
           </div>
@@ -63,14 +115,14 @@ function DisplayData() {
         movieData.movies &&
         movieData.movies.map((movie) => (
           <div key={movie.id}>
-            <h3>Movie Name: {movie.name}</h3>
+            <p>{movie.name}</p>
           </div>
         ))}
 
       <div>
         <input
           type="text"
-          placeholder="movie name..."
+          placeholder="movie name...(case sensitive)"
           onChange={(event) => {
             setMovieSearched(event.target.value);
           }}
@@ -86,10 +138,10 @@ function DisplayData() {
           {movieSearchedData && (
             <div>
               {" "}
-              <h3>Movie Name: {movieSearchedData.movie.name}</h3>
-              <h3>
+              <p>Movie Name: {movieSearchedData.movie.name}</p>
+              <p>
                 Year of Publication: {movieSearchedData.movie.yearOfPublication}
-              </h3>
+              </p>
             </div>
           )}
         </div>
