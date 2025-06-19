35844cdeaeeaaeebe8997cf4d859e5a3c65761f1
Emely Pleitez <epleitez@belaytech.com>
2025-06-19 11:05:46 -0400

    initial commit - graphql basics + apollo

---
+++ server/schema/resolvers.js
@@ -0,0 +1,69 @@
+const { UserList, MovieList } = require("../FakeData");
+const _ = require("lodash");
+
+const resolvers = {
+  Query: {
+    //User resolvers
+    users: () => {
+      return UserList;
+    },
+
+    user: (parent, args) => {
+      const id = args.id;
+      const user = _.find(UserList, { id: Number(id) });
+      return user;
+    },
+
+    //Movie resolvers
+    movies: () => {
+      return MovieList;
+    },
+
+    movie: (parent, args) => {
+      const name = args.name;
+      const movie = _.find(MovieList, { name });
+      return movie;
+    },
+  },
+
+  User: {
+    favoriteMovies: () => {
+      return _.filter(
+        MovieList,
+        (movie) =>
+          movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
+      );
+    },
+  },
+
+  Mutation: {
+    createUser: (parent, args) => {
+      const user = args.input;
+      const lastId = UserList[UserList.length - 1].id;
+      user.id = lastId + 1;
+      UserList.push(user);
+      return user;
+    },
+    updateUsername: (parent, args) => {
+      // const id = args.input.id;
+      // const newUsername = args.input.newUsername;
+      const { id, newUsername } = args.input;
+      let userUpdated;
+      UserList.forEach((user) => {
+        if (user.id === Number(id)) {
+          user.username = newUsername;
+          userUpdated = user;
+        }
+      });
+      return userUpdated;
+    },
+
+    deleteUser: (parent, args) => {
+      const id = args.id;
+      _.remove(UserList, (user) => user.id === Number(id));
+      return null; // Returning null as per GraphQL convention for delete mutations
+    },
+  },
+};
+
+module.exports = { resolvers };
